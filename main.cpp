8847e23bc4f110b4460c9088372e2d0de1272a75
Borixx33 <florian.borie@ynov.com>
2019-03-21 10:34:30 +0100

    update file main

--- main.cpp
+++ main.cpp
@@ -1,72 +1,104 @@
-/*
- * Copyright (c) 2017, CATIE, All Rights Reserved
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 #include "mbed.h"
-#include "rtos.h"
+#include "zest-radio-atzbrf233.h"
+#include "MQTTNetwork.h"
+#include "MQTTmbed.h"
+#include "MQTTClient.h"
 
-namespace {
-#define PERIOD_MS 500
-}
+// Network interface
+NetworkInterface *net;
 
-static DigitalOut led1(LED1);
-Thread thread_ping;
-Thread thread_pong;
-Mutex mutex;
-Semaphore semaphore(2);
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
+int arrivedcount = 0;
+const char* topic = "/YNOV/messages";
 
-void ping()
-    {
-
-	int cpt = 0;
-
-	while(cpt < 10){
-		mutex.lock();
-    	printf("Ping\n");
-    	semaphore.release();
-    	mutex.unlock();
-    	cpt++;
-		}
-	}
-
-void pong()
+/* Printf the message received and its configuration */
+void messageArrived(MQTT::MessageData& md)
 {
-
-	int cpt = 0;
-
-	while(cpt < 10){
-		mutex.lock();
-		semaphore.wait();
-		printf("Pong\n");
-		wait(1);
-		mutex.unlock();
-	cpt++;;
-	}
+    MQTT::Message &message = md.message;
+    printf("Message arrived: qos %d, retained %d, dup %d, packetid %d\r\n", message.qos, message.retained, message.dup, message.id);
+    printf("Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
+    ++arrivedcount;
 }
 
-int main()
-{
-	thread_ping.start(ping);
-	thread_pong.start(pong);
-	thread_ping.join();
-	thread_pong.join();
-	printf("Terminer");
+// MQTT demo
+int main() {
+	int result;
+
+    // Add the border router DNS to the DNS table
+    nsapi_addr_t new_dns = {
+        NSAPI_IPv6,
+        { 0xfd, 0x9f, 0x59, 0x0a, 0xb1, 0x58, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x01 }
+    };
+    nsapi_dns_add_server(new_dns);
+
+    printf("Starting MQTT demo\n");
+
+    // Get default Network interface (6LowPAN)
+    net = NetworkInterface::get_default_instance();
+    if (!net) {
+        printf("Error! No network inteface found.\n");
+        return 0;
+    }
+
+    // Connect 6LowPAN interface
+    result = net->connect();
+    if (result != 0) {
+        printf("Error! net->connect() returned: %d\n", result);
+        return result;
+    }
+
+    // Build the socket that will be used for MQTT
+    MQTTNetwork mqttNetwork(net);
+
+    // Declare a MQTT Client
+    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
+
+    // Connect the socket to the MQTT Broker
+    const char* hostname = "fd9f:590a:b158:ffff:ffff::c0a8:0103";
+    uint16_t port = 1883;
+    printf("Connecting to %s:%d\r\n", hostname, port);
+    int rc = mqttNetwork.connect(hostname, port);
+    if (rc != 0)
+        printf("rc from TCP connect is %d\r\n", rc);
+
+    // Connect the MQTT Client
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = "mbed-sample";
+    data.username.cstring = "floborie";
+    data.password.cstring = "";
+    if ((rc = client.connect(data)) != 0)
+        printf("rc from MQTT connect is %d\r\n", rc);
+
+    // Subscribe to the same topic we will publish in
+    if ((rc = client.subscribe(topic, MQTT::QOS2, messageArrived)) != 0)
+        printf("rc from MQTT subscribe is %d\r\n", rc);
+
+    // Send a message with QoS 0
+    MQTT::Message message;
+
+    // QoS 0
+    char buf[100];
+    sprintf(buf, "Hello World!  QoS 0 message from 6TRON\r\n");
+
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*)buf;
+    message.payloadlen = strlen(buf)+1;
+
+    rc = client.publish(topic, message);
+
+    // yield function is used to refresh the connexion
+    // Here we yield until we receive the message we sent
+    while (arrivedcount < 1)
+        client.yield(100);
+
+    // Disconnect client and socket
+    client.disconnect();
+    mqttNetwork.disconnect();
+
+    // Bring down the 6LowPAN interface
+    net->disconnect();
+    printf("Done\n");
 }
-
-
-
 
